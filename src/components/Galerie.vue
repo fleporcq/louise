8d75ce0729a151259ec7c84e3df2ab319861ff04
François LEPORCQ <fleporcq@gmail.com>
2017-11-19 21:52:41 +0100

    feat: gestion des tags

--- src/components/Galerie.vue
+++ src/components/Galerie.vue
@@ -1,3 +1,19 @@
 <template>
-<h1>Galerie</h1>
+  <div>
+    <h1>Galerie</h1>
+    <ul v-for="tag in tags">
+      <li>{{tag.label}}</li>
+    </ul>
+  </div>
 </template>
+
+<script>
+import {db} from '../firebase'
+
+export default {
+  name: 'Galerie',
+  firebase: {
+    tags: db.ref('tags')
+  }
+}
+</script>
---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import Firebase from 'firebase'
+
+const firebaseApp = Firebase.initializeApp({
+  apiKey: 'AIzaSyBoyx-PQabdy8VPUHG1toe1Wozs5Jd1UQc',
+  authDomain: 'louise-99469.firebaseapp.com',
+  databaseURL: 'https://louise-99469.firebaseio.com',
+  projectId: 'louise-99469',
+  storageBucket: 'louise-99469.appspot.com',
+  messagingSenderId: '470963597272'
+})
+
+export const db = firebaseApp.database()

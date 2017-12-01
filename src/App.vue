4c8432e04ff83b54b29acc075756e6be0d14c10a
François LEPORCQ <fleporcq@gmail.com>
2017-12-01 15:55:33 +0100

    refactor: extraction du layout global

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,69 @@
 <template>
-  <div>
-    <router-view/>
+  <div class="o-grid o-grid--no-gutter o-grid--xsmall-full o-grid--small-full o-grid--medium-full o-grid--large-full">
+    <div class="o-grid__cell o-grid__cell--width-fixed main-cell">
+      <div class="position-fixed">
+        <div class="brand">
+          <h1 class="brand__name">{{profile.first_name}}</h1>
+          <h2 class="brand__baseline">{{profile.occupation}}</h2>
+        </div>
+        <nav class="menu">
+          <h3 class="menu__title">WORKS</h3>
+          <ul class="menu__tags">
+            <li>
+              #&nbsp;<router-link :to="{name: 'gallery-all'}">all</router-link>
+            </li>
+            <li v-for="tag in tags">
+              #&nbsp;<router-link :to="{ name: 'gallery-search', params: { tag: tag._content }}">{{tag._content}}</router-link>
+            </li>
+          </ul>
+          <h3 class="menu__title">ABOUT</h3>
+          <h3 class="menu__title">CONTACT</h3>
+        </nav>
+      </div>
+    </div>
+    <div class="o-grid__cell">
+      <router-view/>
+    </div>
   </div>
 </template>
+<script>
+  import Flickr from './services/Flickr'
+
+  export default {
+    data () {
+      return {
+        profile: {},
+        tags: []
+      }
+    },
+    computed: {
+      flickr: () => new Flickr('4e5407323228a7c7851ce6d7d650a15b', '154566401@N05')
+    },
+    created () {
+      this.fetchTags()
+      this.getProfile()
+    },
+    methods: {
+      getProfile () {
+        this.flickr.getProfile()
+          .then(response => {
+            this.profile = response.profile
+          })
+      },
+      fetchTags () {
+        this.flickr.getUserTags()
+          .then(response => {
+            this.tags = response.who.tags.tag
+          })
+      }
+    }
+  }
+</script>
 <style>
   @import '../node_modules/blaze/dist/blaze.min.css';
   @import url('https://fonts.googleapis.com/css?family=Lato');
 </style>
 <style lang="styl">
-  @import 'assets/variables.styl';
-  @import 'assets/app.styl';
+  @import './assets/variables.styl';
+  @import './assets/app.styl';
 </style>
--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,39 +1,14 @@
 <template>
-  <div class="o-grid o-grid--no-gutter o-grid--xsmall-full o-grid--small-full o-grid--medium-full o-grid--large-full">
-      <div class="o-grid__cell o-grid__cell--width-fixed main-cell">
-        <div class="position-fixed">
-          <div class="brand">
-            <h1 class="brand__name">{{profile.first_name}}</h1>
-            <h2 class="brand__baseline">{{profile.occupation}}</h2>
+    <div class="masonry">
+      <div class="masonry__item" v-for="photo in photos">
+        <div class="photo">
+          <img :src="photo.src" :key="photo.id">
+          <div class="photo__overlay">
+            <div class="photo__overlay__title">{{photo.title}}</div>
           </div>
-          <nav class="menu">
-            <h3 class="menu__title">WORKS</h3>
-            <ul class="menu__tags">
-              <li>
-                #&nbsp;<router-link :to="{name: 'gallery-all'}">all</router-link>
-              </li>
-              <li v-for="tag in tags">
-                #&nbsp;<router-link :to="{ name: 'gallery-search', params: { tag: tag._content }}">{{tag._content}}</router-link>
-              </li>
-            </ul>
-            <h3 class="menu__title">ABOUT</h3>
-            <h3 class="menu__title">CONTACT</h3>
-          </nav>
         </div>
       </div>
-      <div class="o-grid__cell">
-          <div class="masonry">
-            <div class="masonry__item" v-for="photo in photos">
-              <div class="photo">
-                <img :src="photo.src" :key="photo.id">
-                <div class="photo__overlay">
-                  <div class="photo__overlay__title">{{photo.title}}</div>
-                </div>
-              </div>
-            </div>
-          </div>
-      </div>
-  </div>
+    </div>
 </template>
 
 <script>
@@ -43,8 +18,6 @@
   export default {
     data () {
       return {
-        profile: null,
-        tags: [],
         photos: []
       }
     },
@@ -52,10 +25,7 @@
       flickr: () => new Flickr('4e5407323228a7c7851ce6d7d650a15b', '154566401@N05')
     },
     created () {
-      this.fetchTags()
-      let tag = this.$route.params.tag
-      this.getProfile()
-      this.fetchPhotos(tag)
+      this.fetchPhotos(this.$route.params.tag)
     },
     watch: {
       '$route' (to, from) {
@@ -63,18 +33,6 @@
       }
     },
     methods: {
-      getProfile () {
-        this.flickr.getProfile()
-          .then(response => {
-            this.profile = response.profile
-          })
-      },
-      fetchTags () {
-        this.flickr.getUserTags()
-          .then(response => {
-            this.tags = response.who.tags.tag
-          })
-      },
       fetchPhotos (tag) {
         this.flickr.searchPhotos({
           tags: tag === undefined ? '' : tag,
@@ -97,6 +55,5 @@
 </script>
 <style lang="styl" scoped>
   @import '../assets/variables.styl';
-  @import '../assets/masonry.styl';
   @import '../assets/gallery.styl';
 </style>
